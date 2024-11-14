d084a234111d844cc5ed2a240f37f7be91573a73
Javier Estrada <javiera.estradag@gmail.com>
2024-11-14 15:16:00 -0600

    Se cambio el nombre de Ventas a Carrito

--- MenuPrincipal.Designer.cs
+++ MenuPrincipal.Designer.cs
@@ -56,9 +56,8 @@
             panel1.Controls.Add(btnCerrar);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
-            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1257, 47);
+            panel1.Size = new Size(1100, 35);
             panel1.TabIndex = 5;
             // 
             // lblUsuario
@@ -66,9 +65,9 @@
             lblUsuario.AutoSize = true;
             lblUsuario.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblUsuario.ForeColor = Color.White;
-            lblUsuario.Location = new Point(1010, 12);
+            lblUsuario.Location = new Point(884, 9);
             lblUsuario.Name = "lblUsuario";
-            lblUsuario.Size = new Size(87, 22);
+            lblUsuario.Size = new Size(68, 18);
             lblUsuario.TabIndex = 12;
             lblUsuario.Text = "lblUsuario";
             // 
@@ -77,9 +76,9 @@
             label4.AutoSize = true;
             label4.Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.White;
-            label4.Location = new Point(14, 12);
+            label4.Location = new Point(12, 9);
             label4.Name = "label4";
-            label4.Size = new Size(248, 23);
+            label4.Size = new Size(193, 18);
             label4.TabIndex = 11;
             label4.Text = "Sistema Gestión de Servicios";
             // 
@@ -90,10 +89,9 @@
             btnCerrar.FlatStyle = FlatStyle.Flat;
             btnCerrar.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnCerrar.ForeColor = Color.White;
-            btnCerrar.Location = new Point(1211, 0);
-            btnCerrar.Margin = new Padding(3, 4, 3, 4);
+            btnCerrar.Location = new Point(1060, 0);
             btnCerrar.Name = "btnCerrar";
-            btnCerrar.Size = new Size(46, 47);
+            btnCerrar.Size = new Size(40, 35);
             btnCerrar.TabIndex = 10;
             btnCerrar.Text = "X";
             btnCerrar.UseVisualStyleBackColor = false;
@@ -112,10 +110,9 @@
             panel2.Controls.Add(lblNombre);
             panel2.Controls.Add(pictureBox1);
             panel2.Dock = DockStyle.Left;
-            panel2.Location = new Point(0, 47);
-            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Location = new Point(0, 35);
             panel2.Name = "panel2";
-            panel2.Size = new Size(257, 886);
+            panel2.Size = new Size(225, 665);
             panel2.TabIndex = 6;
             // 
             // label6
@@ -123,9 +120,9 @@
             label6.AutoSize = true;
             label6.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(53, 833);
+            label6.Location = new Point(46, 625);
             label6.Name = "label6";
-            label6.Size = new Size(53, 23);
+            label6.Size = new Size(44, 18);
             label6.TabIndex = 13;
             label6.Text = "Salir";
             // 
@@ -135,10 +132,9 @@
             btnSignOut.FlatAppearance.BorderSize = 0;
             btnSignOut.FlatStyle = FlatStyle.Flat;
             btnSignOut.Image = Properties.Resources.account_logout_24;
-            btnSignOut.Location = new Point(14, 827);
-            btnSignOut.Margin = new Padding(3, 4, 3, 4);
+            btnSignOut.Location = new Point(12, 620);
             btnSignOut.Name = "btnSignOut";
-            btnSignOut.Size = new Size(27, 32);
+            btnSignOut.Size = new Size(24, 24);
             btnSignOut.TabIndex = 7;
             btnSignOut.UseVisualStyleBackColor = true;
             btnSignOut.Click += btnSignOut_Click;
@@ -153,10 +149,9 @@
             btnEmpleados.ForeColor = Color.White;
             btnEmpleados.Image = Properties.Resources.administrator_2_32;
             btnEmpleados.ImageAlign = ContentAlignment.MiddleLeft;
-            btnEmpleados.Location = new Point(14, 607);
-            btnEmpleados.Margin = new Padding(3, 4, 3, 4);
+            btnEmpleados.Location = new Point(12, 455);
             btnEmpleados.Name = "btnEmpleados";
-            btnEmpleados.Size = new Size(229, 53);
+            btnEmpleados.Size = new Size(200, 40);
             btnEmpleados.TabIndex = 12;
             btnEmpleados.Text = "Empleados";
             btnEmpleados.UseVisualStyleBackColor = false;
@@ -172,10 +167,9 @@
             btnClientes.ForeColor = Color.White;
             btnClientes.Image = Properties.Resources.group_32;
             btnClientes.ImageAlign = ContentAlignment.MiddleLeft;
-            btnClientes.Location = new Point(14, 528);
-            btnClientes.Margin = new Padding(3, 4, 3, 4);
+            btnClientes.Location = new Point(12, 396);
             btnClientes.Name = "btnClientes";
-            btnClientes.Size = new Size(229, 53);
+            btnClientes.Size = new Size(200, 40);
             btnClientes.TabIndex = 11;
             btnClientes.Text = "Clientes";
             btnClientes.UseVisualStyleBackColor = false;
@@ -191,10 +185,9 @@
             btnProductos.ForeColor = Color.White;
             btnProductos.Image = Properties.Resources.shopping_basket_32;
             btnProductos.ImageAlign = ContentAlignment.MiddleLeft;
-            btnProductos.Location = new Point(14, 448);
-            btnProductos.Margin = new Padding(3, 4, 3, 4);
+            btnProductos.Location = new Point(12, 336);
             btnProductos.Name = "btnProductos";
-            btnProductos.Size = new Size(229, 53);
+            btnProductos.Size = new Size(200, 40);
             btnProductos.TabIndex = 10;
             btnProductos.Text = "Productos";
             btnProductos.UseVisualStyleBackColor = false;
@@ -210,12 +203,11 @@
             btnVentas.ForeColor = Color.White;
             btnVentas.Image = Properties.Resources.cart_59_32;
             btnVentas.ImageAlign = ContentAlignment.MiddleLeft;
-            btnVentas.Location = new Point(14, 367);
-            btnVentas.Margin = new Padding(3, 4, 3, 4);
+            btnVentas.Location = new Point(12, 275);
             btnVentas.Name = "btnVentas";
-            btnVentas.Size = new Size(229, 53);
+            btnVentas.Size = new Size(200, 40);
             btnVentas.TabIndex = 9;
-            btnVentas.Text = "Ventas";
+            btnVentas.Text = "Carrito";
             btnVentas.UseVisualStyleBackColor = false;
             btnVentas.Click += btnVentas_Click_1;
             // 
@@ -229,10 +221,9 @@
             btnDashboard.ForeColor = Color.White;
             btnDashboard.Image = Properties.Resources.dashboard_2_32;
             btnDashboard.ImageAlign = ContentAlignment.MiddleLeft;
-            btnDashboard.Location = new Point(14, 285);
-            btnDashboard.Margin = new Padding(3, 4, 3, 4);
+            btnDashboard.Location = new Point(12, 214);
             btnDashboard.Name = "btnDashboard";
-            btnDashboard.Size = new Size(229, 53);
+            btnDashboard.Size = new Size(200, 40);
             btnDashboard.TabIndex = 8;
             btnDashboard.Text = "Dashboard";
             btnDashboard.UseVisualStyleBackColor = false;
@@ -243,40 +234,41 @@
             lblNombre.AutoSize = true;
             lblNombre.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblNombre.ForeColor = Color.White;
-            lblNombre.Location = new Point(35, 220);
+            lblNombre.Location = new Point(31, 165);
             lblNombre.Name = "lblNombre";
-            lblNombre.Size = new Size(185, 24);
+            lblNombre.Size = new Size(148, 19);
             lblNombre.TabIndex = 7;
             lblNombre.Text = "Bienvenido, usuario";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.logoLogin;
-            pictureBox1.Location = new Point(14, 36);
-            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Location = new Point(12, 27);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(229, 119);
+            pictureBox1.Size = new Size(200, 89);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // panelContainer
             // 
             panelContainer.Dock = DockStyle.Fill;
-            panelContainer.Location = new Point(257, 47);
+            panelContainer.Location = new Point(225, 35);
+            panelContainer.Margin = new Padding(3, 2, 3, 2);
             panelContainer.Name = "panelContainer";
-            panelContainer.Size = new Size(1000, 886);
+            panelContainer.Size = new Size(875, 665);
             panelContainer.TabIndex = 7;
             // 
             // MenuPrincipal
             // 
             AccessibleRole = AccessibleRole.TitleBar;
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1257, 933);
+            ClientSize = new Size(1100, 700);
             Controls.Add(panelContainer);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "MenuPrincipal";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Menu Principal";
